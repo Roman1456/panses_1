66c42d6cbe776f456f932a1e733d7836ba108e56
Lpopivnyak <stadnykroman2010@gmail.com>
2024-10-22 16:17:19 +0300

    vbcvbcv

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import pandas as pd
 
-df = pd.read_csv('GoogleApps (3).csv')
+df = pd.read_csv('GooglePlayStore_wild123.csv')
 
-#print(df[df['Category'] == 'ART_AND_DESIGN']['Installs'].median())
 
-free = df[df['Type'] == 'Free']['Reviews'].max()
-paid = df[df['Type'] == 'Paid']['Reviews'].max()
-print(free - paid)
+df['Rating'] = df['Rating'].fillna(-1,inplace = True)
+
+def make_size(prise):
+    if prise[-1] == '$':
+        return float(prise[:-1])
+
+    return -1
+df['Price'] = df['Price'].apply(make_size)
+df['Profit'] = df['Installs'] * df['Price']
+print(df[df['Type'] == 'Paid']['Size'].max)
